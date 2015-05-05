399ab836e287ff5a20f975fde97ec8972da1fcd0
Yihang Xu <yihang.xu@its.ny.gov>
2015-05-05 15:54:49 -0400

    Other scripts
    Signed-off-by: Yihang Xu <yihang.xu@its.ny.gov>

---
+++ scripts/create.bat
@@ -0,0 +1,4 @@
+mvn archetype:generate -DarchetypeArtifactId=jersey-quickstart-webapp \
+                -DarchetypeGroupId=org.glassfish.jersey.archetypes -DinteractiveMode=false \
+                -DgroupId=com.example -DartifactId=simple-service-webapp -Dpackage=com.example \
+                -DarchetypeVersion=2.7
